bb6eed4b5205acf9bc93a3d975c1a683680c2106
Octavian21 <cotfasa.octavian@yahoo.com>
2020-10-12 14:16:58 +0300

    Hover example in draft

--- draft.py
+++ draft.py
@@ -1,13 +1,28 @@
-from tkinter import *
 
-def motion(event):
-  print("Mouse position: (%s %s)" % (event.x, event.y))
-  return
 
-master = Tk()
-whatever_you_do = "Whatever you do will be insignificant, but it is very important that you do it.(Mahatma Gandhi)"
-msg = Message(master, text = whatever_you_do)
-msg.config(bg='lightgreen', font=('times', 24, 'italic'))
-msg.bind('<Motion>',motion)
-msg.pack()
-mainloop()
+
+
+#hover example
+import tkinter as tk
+
+class Example(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        self.l1 = tk.Label(self, text="Hover over me")
+        self.l2 = tk.Label(self, text="", width=40)
+        self.l1.pack(side="top")
+        self.l2.pack(side="top", fill="x")
+
+        self.l1.bind("<Enter>", self.on_enter)
+        self.l1.bind("<Leave>", self.on_leave)
+
+    def on_enter(self, event):
+        self.l2.configure(text="Hello world")
+
+    def on_leave(self, enter):
+        self.l2.configure(text="")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    Example(root).pack(side="top", fill="both", expand="true")
+    root.mainloop()
